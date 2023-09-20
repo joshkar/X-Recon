94ba2792257ef15ce10b9e7bc84bf697339c7469
joshkar <qadir.yolme@gmail.com>
2023-09-20 16:21:32 +0330

    It manages the scraping process and also sends the initial data to engine.py

---
+++ module/core.py
@@ -0,0 +1,45 @@
+from module.validate import http_checker
+from module.engine import urls,result,scrape,get_inputs_names_and_ids
+from concurrent.futures import ThreadPoolExecutor
+from module.engine import allowed_domains
+from time import gmtime, strftime
+
+time = strftime("%Y-%m-%d-%H:%M", gmtime())
+
+def start_proccess(file_name,url):
+    allowed_domains.append(http_checker(url,status="remove"))
+    url_file_name = "logs/"+time+"-"+file_name+".txt"
+
+    scrape(url,5)
+    with open(url_file_name,"w") as site_links:
+        for i in urls:
+            if i == "":
+                pass
+
+            else:
+                site_links.write(i+"\n")
+
+
+    data = open(url_file_name,"r").read().split("\n")
+    with ThreadPoolExecutor(max_workers=16) as pool:
+        tr = pool.map(get_inputs_names_and_ids,data)
+        for _ in tr:
+            
+            pass
+
+
+
+    data_result = open(f"logs/{time}-{file_name}-param.txt","a")
+
+    for i in result:
+        try:
+
+            data_result.write(i+"\n")
+        except:
+            pass
+
+    data_result.close()
+
+    print(f"File Saved To : logs/{time}-{file_name}-extracted-param.txt")
+    input("If you Want back to menu > press")
+
