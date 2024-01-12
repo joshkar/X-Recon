f52e9717a30c35eb6a319a467a4081add2a630e0
Mohammad Saleh <scrolsin333@gmail.com>
2024-01-12 13:16:25 +0330

    Update xr.py
    change logs
    add show_menu arg

--- xr.py
+++ xr.py
@@ -7,15 +7,15 @@ import json
 
 while True:
     show_banner()
-    show_menu()
+    show_menu("Choose one of the options below")
     try:
 
-        original_menu = custom_input("HOME")
+        original_menu = custom_input("home")
         
 
         if  original_menu == "1":
             show_banner()
-            print("Ok. Give me a URL\n")
+            print(f"{color.costum(39)}[{color.RESET}?{color.costum(39)}]{color.RESET} Ok. Give me a URL\n")
             get_url = custom_input("GET-URL")
             
             file_name = f"{http_checker(get_url,status='remove')}"
@@ -27,19 +27,19 @@ while True:
             show_banner()
             xss_files = find_scanned_files()
             if not xss_files:
-                exit("No File For Scan")
+                exit(f"[{color.costum(124)}Notification{color.RESET}] No File For Scan")
             
             else:
 
                 num_x = 0
-                print(" Select A file \n ")
-                for i in xss_files:    
-                    print(f"{Fore.WHITE} [{num_x}] {Fore.RED}{i}\n")
+                print(f"{color.costum(39)}[{color.RESET}?{color.costum(39)}]{color.RESET} Select A file\n")
+                for i in xss_files:
+                    print(f"{color.costum(39)}[{color.RESET}{num_x}{color.costum(39)}]{color.RESET} {i}")
                     num_x +=1
                 
                 xss_file_id = int(custom_input("GET-FILE"))
                 xss_file_id = "logs/"+xss_files[xss_file_id]
-                print("Testing XSS . . .")
+                print(f"[{color.costum(124)}Notification{color.RESET}] Testing XSS ...")
                 start_scan(xss_file_id)
                 
         
@@ -51,15 +51,15 @@ while True:
 
         elif original_menu == "4":
             
-            exit("\n Goodbye :) ")
+            exit(f"\n[{color.costum(124)}Notification{color.RESET}] Goodbye :) ")
         
             
 
 
 
     except KeyboardInterrupt:
-        exit("\n Goodbye :) ")
+        exit(f"\n[{color.costum(124)}Notification{color.RESET}] Goodbye :) ")
 
     except ValueError:
-        exit("\n Please Enter a Number")
+        exit(f"\n[{color.costum(124)}Notification{color.RESET}] Please Enter a Number ")
 
